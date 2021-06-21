e1ca19d53d6f1d77499ea7696c96d7b7436285dc
jgabriel1 <josegabriel@det.ufc.br>
2021-06-21 17:03:35 -0300

    Added error handling middleware to catch AppError and handle responses appropriately.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,11 @@
 import 'reflect-metadata';
 
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
+
 import { connectToDatabase } from './database/connection';
 import { router } from './routes';
+import { AppError } from './errors/AppError';
 
 connectToDatabase();
 
@@ -10,6 +13,21 @@ const app = express();
 
 app.use(express.json());
 app.use(router);
+
+app.use(
+  (error: Error, req: Request, res: Response, next: NextFunction): Response => {
+    if (error instanceof AppError)
+      return res.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+
+    return res.status(500).json({
+      status: 'error',
+      message: `Internal server error ${error.message}`,
+    });
+  }
+);
 
 const PORT = 3333;
 
