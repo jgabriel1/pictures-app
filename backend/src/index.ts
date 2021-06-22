46f61988c46063b38e670fc9799ab4bb0f769ac6
jgabriel1 <josegabriel@det.ufc.br>
2021-06-22 14:53:22 -0300

    Abstracted error handling middleware.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,10 @@
 import 'reflect-metadata';
-import express, { NextFunction, Request, Response } from 'express';
+import express from 'express';
 import 'express-async-errors';
 import { connectToDatabase } from './database/connection';
 import { router } from './routes';
-import { AppError } from './errors/AppError';
 import config from './config';
+import { errorHandlingMiddleware } from './middlewares/errorHandlingMiddleware';
 
 connectToDatabase();
 
@@ -13,21 +13,7 @@ const app = express();
 app.use('/static', express.static(config.staticFolder));
 app.use(express.json());
 app.use(router);
-
-app.use(
-  (error: Error, req: Request, res: Response, next: NextFunction): Response => {
-    if (error instanceof AppError)
-      return res.status(error.statusCode).json({
-        status: 'error',
-        message: error.message,
-      });
-
-    return res.status(500).json({
-      status: 'error',
-      message: `Internal server error ${error.message}`,
-    });
-  }
-);
+app.use(errorHandlingMiddleware);
 
 const PORT = 3333;
 
