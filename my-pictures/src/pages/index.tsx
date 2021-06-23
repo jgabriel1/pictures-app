e94e7a5c4a8e736e8e265e1e5e8dbf53e9cd0339
jgabriel1 <josegabriel@det.ufc.br>
2021-06-22 21:31:53 -0300

    Created logon form.

--- my-pictures/src/pages/index.tsx
+++ my-pictures/src/pages/index.tsx
@@ -1,18 +1,39 @@
-import { Button, Flex, Heading, VStack } from '@chakra-ui/react';
-import { useRouter } from 'next/dist/client/router';
-import React from 'react';
+import {
+  Button,
+  Flex,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Heading,
+  Input,
+  VStack,
+} from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import { useForm } from 'react-hook-form';
 import { CenterContainer } from '../components/CenterContainer';
-import { FormInput } from '../components/FormInput';
+
+interface LogonFormData {
+  email: string;
+  password: string;
+}
 
 export default function Home() {
   const router = useRouter();
+
+  const { register, handleSubmit, formState } = useForm<LogonFormData>();
 
   const handleNavigateToSignUp = () => {
     router.push('signup');
   };
 
+  const handleSubmitLogonForm = handleSubmit(data => {
+    console.log(data);
+
+    return new Promise(resolve => setTimeout(() => resolve(data), 1000));
+  });
+
   return (
-    <CenterContainer>
+    <CenterContainer as="form" onSubmit={handleSubmitLogonForm}>
       <Flex flexDir="column" mb="8" align="center">
         <Heading as="h2" fontSize="4xl">
           my.Pictures
@@ -20,9 +41,29 @@ export default function Home() {
       </Flex>
 
       <VStack spacing="4">
-        <FormInput label="E-Mail" isRequired />
+        <FormControl id="email" isInvalid={!!formState.errors.email}>
+          <FormLabel>E-Mail</FormLabel>
 
-        <FormInput label="Senha" isRequired />
+          <Input type="email" {...register('email', { required: true })} />
+
+          <FormErrorMessage>
+            {formState.errors.email?.type === 'required' && 'Campo Obrigatório'}
+          </FormErrorMessage>
+        </FormControl>
+
+        <FormControl id="password" isInvalid={!!formState.errors.password}>
+          <FormLabel>Senha</FormLabel>
+
+          <Input
+            type="password"
+            {...register('password', { required: true })}
+          />
+
+          <FormErrorMessage>
+            {formState.errors.password?.type === 'required' &&
+              'Campo Obrigatório'}
+          </FormErrorMessage>
+        </FormControl>
       </VStack>
 
       <Flex w="100%" justify="space-between" align="center" mt="8">
@@ -30,7 +71,12 @@ export default function Home() {
           Cadastre-se
         </Button>
 
-        <Button size="lg" colorScheme="blue" type="submit">
+        <Button
+          size="lg"
+          colorScheme="blue"
+          type="submit"
+          isLoading={formState.isSubmitting}
+        >
           Entrar
         </Button>
       </Flex>
