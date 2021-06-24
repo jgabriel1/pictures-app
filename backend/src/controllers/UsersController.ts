a1e013892e59cc2ba6d0d0f9a8cc16f7c04eb0fe
jgabriel1 <josegabriel@det.ufc.br>
2021-06-24 19:59:26 -0300

    Backend: Created route to show user information when authenticatied /users/me

--- backend/src/controllers/UsersController.ts
+++ backend/src/controllers/UsersController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { RegisterUserService } from '../services/users';
+import { ShowUserDataService } from '../services/users/ShowUserDataService';
 
 export class UsersController {
   public async create(req: Request, res: Response) {
@@ -14,5 +15,19 @@ export class UsersController {
     });
 
     return res.status(201).json({ user });
+  }
+
+  public async show(req: Request, res: Response) {
+    const { user_id } = req;
+
+    console.log(user_id);
+
+    const showUserData = new ShowUserDataService();
+
+    const user = await showUserData.execute({
+      user_id: user_id || '',
+    });
+
+    return res.json({ user });
   }
 }
--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -1,13 +1,17 @@
 import { Router } from 'express';
 import { UsersController } from '../controllers/UsersController';
 import { AuthenticationController } from '../controllers/AuthenticationController';
+import { checkAuthenticationMiddleware } from '../middlewares/checkAuthenticationMiddleware';
 
 const usersController = new UsersController();
 const authenticationController = new AuthenticationController();
 
 const usersRouter = Router();
 
+usersRouter.get('/me', checkAuthenticationMiddleware, usersController.show);
+
 usersRouter.post('/register', usersController.create);
+
 usersRouter.post('/authenticate', authenticationController.create);
 
 export { usersRouter };
