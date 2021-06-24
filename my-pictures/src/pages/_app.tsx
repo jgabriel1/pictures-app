82d629b1a4eade66e17e8b944c7cee5942bd8db0
jgabriel1 <josegabriel@det.ufc.br>
2021-06-24 20:15:08 -0300

    Added guaranteed dark theme for entire application regardless of system configurations.

--- my-pictures/src/pages/_app.tsx
+++ my-pictures/src/pages/_app.tsx
@@ -2,13 +2,14 @@ import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { AppProvider } from '../contexts';
+import { theme } from '../styles/theme';
 
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <AppProvider>
           <Component {...pageProps} />
         </AppProvider>
