97c9749884332cf3b292df0450f245ecc84edb5a
jgabriel1 <josegabriel@det.ufc.br>
2021-06-24 21:33:35 -0300

    Added retry logic to the database connection.

--- backend/src/database/connection.ts
+++ backend/src/database/connection.ts
@@ -2,12 +2,30 @@ import path from 'path';
 import { createConnection } from 'typeorm';
 
 export const connectToDatabase = async () => {
-  await createConnection({
-    type: 'sqlite',
-    name: 'default',
-    database: './database.sqlite',
-    logging: true,
-    entities: [path.resolve(__dirname, '..', 'models', '*.ts')],
-    synchronize: true,
-  });
+  let retries = 5;
+
+  while (retries) {
+    try {
+      await createConnection({
+        type: 'sqlite',
+        name: 'default',
+        database: './database.sqlite',
+        logging: true,
+        entities: [path.resolve(__dirname, '..', 'models', '*.ts')],
+        synchronize: true,
+      });
+
+      console.log('CONNECTED TO DATABASE.');
+
+      break;
+    } catch (error) {
+      console.log('THERE WAS AN ERROR CONNECTING, RETRYING IN 5 SECONDS.');
+
+      retries -= 1;
+
+      await new Promise(resolve => {
+        setTimeout(resolve, 5000);
+      });
+    }
+  }
 };
